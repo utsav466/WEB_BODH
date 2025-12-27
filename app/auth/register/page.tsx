e53d62b491f18b9603813d82cffcc7b62f0efa8a
utsav466 <utsavthapa901@gmail.com>
2025-12-27 22:41:18 +0545

    Sprint-1: completed

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,26 +1,26 @@
-// 
+"use client";
+
 import RegisterForm from "../components/RegisterForm";
 
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex">
-      {/* Left  */}
-      <div className="hidden md:flex flex-col justify-center items-center w-1/2 bg-blue-600 p-8">
+      {/* Left Branding Section */}
+      <div className="hidden md:flex flex-col justify-center items-center w-1/2 bg-blue-600 p-12">
         <img
           src="/logo_varient_2.png"
           alt="BODH Logo"
-          width={400}
-          height={400}
-         
-       
+          width={300}
+          height={300}
+          className="mb-6"
         />
-        <p className="-mt-30 text-lg md:text-xl opacity-90 text-center max-w-sm text-white">
+        <p className="text-lg md:text-xl opacity-90 text-center max-w-sm text-white">
           Your digital bookshelf, just a login away.
         </p>
       </div>
 
-      {/* Right form panel */}
-      <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-8">
+      {/* Right Form Section */}
+      <div className="flex flex-col justify-center items-center w-full md:w-1/2 bg-white p-12 shadow-lg">
         <RegisterForm />
       </div>
     </div>
